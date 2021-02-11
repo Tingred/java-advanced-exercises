8c8011ebeb9d5131f4766ef7cce4350a031a2755
Tingred <exiles1@wp.pl>
2021-02-11 18:35:37 +0100

    Interfaces

---
+++ src/main/java/interfaces/Bank.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface Bank {
+
+    String credit (int amount);
+    String debit (int amount);
+    int balance();
+
+}
---
+++ src/main/java/interfaces/Printable.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface Printable {
+
+    String print(String device);
+
+
+}

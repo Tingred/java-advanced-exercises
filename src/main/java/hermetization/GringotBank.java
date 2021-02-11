bf3ebd6f8e0d7779425c0c4f4b934bf4b4a5d18e
Tingred <exiles1@wp.pl>
2021-02-11 17:16:59 +0100

    Hermetyzacja

---
+++ src/main/java/hermetization/GringotBank.java
@@ -0,0 +1,37 @@
+package hermetization;
+
+//Aby nie można było podmienić w miejsce GringotBank klasy wyprowadzonej z niej
+//należy dodać final przed klasą
+// final public class GringotBank extends Bank {
+
+public class GringotBank extends Bank {
+
+    private int balance;
+
+    @Override
+    public String credit(int amount) {
+        if (amount < 0) {
+            return "No change. Invalid amount";
+        }
+        balance +=amount;
+        return "Account changed";
+    }
+
+    @Override
+    public String debit(int amount) {
+        if (amount < 0) {
+            return "No change. Invalid amount";
+        }
+        if (balance - amount <0){
+            return "No change. Not enough money.";
+        }
+        balance -=amount;
+        return "Account changed";
+    }
+
+
+    @Override
+    public int balance() {
+        return balance;
+    }
+}
---
+++ src/main/java/hermetization/SuperHackerBank.java
@@ -0,0 +1,13 @@
+package hermetization;
+//Klasa HermesBank nie zapewnia hermetyzacji bo można dostać się do pola banace
+// i doprowadzić do ujemnej wartości
+public class SuperHackerBank extends HermesBank{
+
+    //Nadpisujemy metodę, aby doprowadzić do ujemnego stanu konta
+
+    @Override
+    public String debit(int amount) {
+        balance -= amount;
+        return "Yes";
+    }
+}

ac5aad61d7f9d3c4798ebc2d8243a6480e8e66ca
Tingred <exiles1@wp.pl>
2021-02-11 21:32:31 +0100

    Klasy wewnętrzne i zagnieżdzone

---
+++ src/main/java/inner/CarDemo.java
@@ -0,0 +1,12 @@
+package inner;
+
+public class CarDemo {
+
+    public static void main(String[] args) {
+
+        Car.Radio radio = new Car.Radio(1200);
+        Car fiat = new Car();
+        fiat.setRadio(radio);
+        System.out.println(fiat);
+    }
+}

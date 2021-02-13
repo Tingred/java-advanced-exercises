925609b016a0a20eba8a42c62e6fd54957a08ff8
Tingred <exiles1@wp.pl>
2021-02-13 14:05:33 +0100

    Wyjątki ćwiczenia

--- src/main/java/exceptions/ExceptionExample.java
+++ src/main/java/exceptions/ExceptionExample.java
@@ -1,22 +1,42 @@
 package exceptions;
 
+import java.time.LocalDate;
+
 public class ExceptionExample {
 
     public static int avg(int ...arg){
-
         int sum = 0;
-        for (int l:arg){
-            sum+=l;
+        for(int l: arg){
+            sum += l;
         }
-        return sum/ arg.length;
-    }
-    public static void main(String[] args) {
-        try{
-            System.out.println(avg());
+        return sum / arg.length;
 
-        }catch (ArithmeticException e){
+    }
+
+    public static double div(double a, double b){
+        if (b == 0.0){
+            throw new ArithmeticException("Dzielenie przez zero! Wynik typu NaN.");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        try {
+            System.out.println(avg());
+        } catch (ArithmeticException e){
             System.out.println("Nie dziel przez zero, lista wartości pusta!!");
         }
 
+        try {
+            //System.out.println(arr[4]);
+            System.out.println(div(0.0, 0.0));
+        } catch (ArithmeticException e){
+            System.out.println("Dzielenie przez zero " + e.getMessage());
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("Wyjście indeksu poza zakres " + e.getMessage());
+        }
+
+        System.out.println((0.0 / 0.0) * 10);
     }
 }
